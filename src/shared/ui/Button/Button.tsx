7d88fc08886cd3ddeeb3d76feda9f4296c8d948d
Roman Mashin <roman.m@red-promo.ru>
2023-02-13 15:46:48 +0500

    initial

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,29 @@
+import cls from './Button.module.scss'
+import {classNames} from "shared/lib/classNames/classNames";
+import {ButtonHTMLAttributes, FC} from "react";
+
+export enum ThemeButton {
+    CLEAR = 'clear',
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+    className?: string;
+    theme?: ThemeButton;
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+    const {
+        children,
+        className,
+        theme,
+        ...otherProps
+    } = props;
+    return (
+        <button
+            className={classNames(cls.Button, {}, [className, cls[theme]])}
+            {...otherProps}
+        >
+            {children}
+        </button>
+    );
+};
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import cls from './Navbar.module.scss'
+import {classNames} from "shared/lib/classNames/classNames";
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+
+interface NavbarProps {
+    className?: string;
+}
+
+export const Navbar = ({className}: NavbarProps) => {
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'}>
+                    Main
+                </AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>
+                    About
+                </AppLink>
+            </div>
+        </div>
+    );
+};
+
