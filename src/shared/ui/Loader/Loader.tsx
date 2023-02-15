a5b3519f4131a782a07bd96f878f2357ea72ca53
Roman Mashin <roman-mashin@yandex.ru>
2023-02-15 18:15:40 +0500

    add loader, not found page

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,13 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Loader.module.scss';
+
+interface LoaderProps {
+    className?: string;
+}
+
+export const Loader = ({ className }: LoaderProps) => (
+    <div className={classNames(cls.ldsRipple, {}, [className])}>
+        <div />
+        <div />
+    </div>
+);
