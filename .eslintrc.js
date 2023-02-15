aae4d490077e29850470352c6560c5e7e03b9dcf
Roman Mashin <roman-mashin@yandex.ru>
2023-02-15 10:13:34 +0500

    eslint hotfix

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,7 +37,7 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'i18next/no-literal-string': ['error', { markupOnly: true, onlyAttribute: [''] }],
     },
     globals: {
         __IS_DEV__: true,
