a2d493f88fb5c85f8dbf74b8c6b2575589ab57a2
Roman Mashin <roman-mashin@yandex.ru>
2023-02-20 23:06:26 +0500

    add storybook

---
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { ThemeSwitcher } from './ThemeSwitcher';
+
+export default {
+    title: 'shared/ThemeSwitcher',
+    component: ThemeSwitcher,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof ThemeSwitcher>;
+
+const Template: ComponentStory<typeof ThemeSwitcher> = (args) => <ThemeSwitcher {...args} />;
+
+export const ThemeSwitcherLight = Template.bind({});
+ThemeSwitcherLight.args = {};
+
+export const ThemeSwitcherDark = Template.bind({});
+ThemeSwitcherDark.args = {};
+ThemeSwitcherDark.decorators = [ThemeDecorator(Theme.DARK)];
