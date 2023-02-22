5a0bc4d68a965c1fe44c4a4fba40dc806a1edb26
Roman Mashin <roman-mashin@yandex.ru>
2023-02-22 19:22:39 +0500

    fix button text

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -25,7 +25,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 data-testid="sidebar-toggle"
                 onClick={onToggle}
             >
-                toggle
+                ---
             </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
