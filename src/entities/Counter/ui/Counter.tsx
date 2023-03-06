eed09f8469c4a42b09f78e83fed06d9308e795e9
Roman Mashin <roman-mashin@yandex.ru>
2023-03-06 19:43:46 +0500

    add redux toolkit, store, counter entity

---
+++ src/entities/Counter/ui/Counter.tsx
@@ -0,0 +1,38 @@
+import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { counterActions } from '../model/slice/counterSlice';
+import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
+
+export const Counter = () => {
+    const dispatch = useDispatch();
+    const value = useSelector(getCounterValue);
+    const increment = () => {
+        dispatch(counterActions.increment());
+    };
+    const decrement = () => {
+        dispatch(counterActions.decrement());
+    };
+    return (
+        <div>
+            <h1 data-testid="value-title">{value}</h1>
+            <Button
+                onClick={increment}
+                theme={ButtonTheme.OUTLINE}
+                square
+                size={ButtonSize.L}
+                data-testid="increment-btn"
+            >
+                +
+            </Button>
+            <Button
+                onClick={decrement}
+                theme={ButtonTheme.OUTLINE}
+                square
+                size={ButtonSize.L}
+                data-testid="decrement-btn"
+            >
+                -
+            </Button>
+        </div>
+    );
+};
