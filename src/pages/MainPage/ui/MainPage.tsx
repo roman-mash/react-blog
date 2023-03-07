312751581d00d5d8aa2d85444a80a342bac24e2f
Roman Mashin <roman-mashin@yandex.ru>
2023-03-07 13:52:41 +0500

    add new input, login modal

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,8 +1,15 @@
 import { useTranslation } from 'react-i18next';
 import { BugButton } from 'app/providers/ErrorBoundary';
+import { Input } from 'shared/ui/Input/Input';
+import { useState } from 'react';
 
 const MainPage = () => {
     const { t } = useTranslation();
+
+    const [value, setValue] = useState('');
+    const onChange = (val: string) => {
+        setValue(val);
+    };
 
     return (
         <div>
