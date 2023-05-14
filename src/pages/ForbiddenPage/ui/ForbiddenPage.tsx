000f7018634ee8b411006c4dfed5d2f516ec4b94
Roman Mashin <roman-mashin@yandex.ru>
2023-05-14 10:51:13 +0500

    add roles, forbidden page, viewing pages by roles

---
+++ src/pages/ForbiddenPage/ui/ForbiddenPage.tsx
@@ -0,0 +1,20 @@
+import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Page } from 'widgets/Page/Page';
+
+interface ForbiddenPageProps {
+    className?: string;
+}
+
+const ForbiddenPage = memo((props: ForbiddenPageProps) => {
+    const { className } = props;
+    const { t } = useTranslation();
+    return (
+        <Page className={classNames('', {}, [className])}>
+            {t('У вас нет доступа к этой странице')}
+        </Page>
+    );
+});
+
+export default ForbiddenPage;
