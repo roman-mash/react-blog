8f20852ab17a8668b980122e24b6b79943c8ecf9
Roman Mashin <roman-mashin@yandex.ru>
2023-05-20 12:13:54 +0500

    add react spring and use gesture, lazy import for libs, gesturing animation for drawer

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -7,6 +7,7 @@ import { Popover } from 'shared/ui/Popups';
 import NotificationIcon from 'shared/assets/icons/notification.svg';
 import { Drawer } from 'shared/ui/Drawer/Drawer';
 import { BrowserView, MobileView } from 'react-device-detect';
+import { AnimationProvider } from 'shared/lib/components/AnimationProvider';
 import cls from './NotificationButton.module.scss';
 
 interface NotificationButtonProps {
@@ -35,9 +36,11 @@ export const NotificationButton = memo((props: NotificationButtonProps) => {
         <>
             <MobileView>
                 {trigger}
-                <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
-                    <NotificationList />
-                </Drawer>
+                <AnimationProvider>
+                    <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+                        <NotificationList />
+                    </Drawer>
+                </AnimationProvider>
             </MobileView>
             <BrowserView>
                 <Popover
