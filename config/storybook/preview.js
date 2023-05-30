0a3654f6da8a25441fe6fbd88574a04dd6aa826a
Roman Mashin <roman-mashin@yandex.ru>
2023-05-30 17:28:40 +0500

    add addon for themes in storybook

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -14,6 +14,14 @@ export const parameters = {
         },
     },
     layout: 'fullscreen',
+    themes: {
+        default: 'light',
+        list: [
+            { name: 'light', class: Theme.LIGHT, color: '#e8e8ea' },
+            { name: 'dark', class: Theme.DARK, color: '#090949' },
+            { name: 'green', class: Theme.GREEN, color: '#16310e' },
+        ],
+    },
 };
 
 addDecorator(StyleDecorator);
