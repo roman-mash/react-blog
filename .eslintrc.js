908d0554f679d6c7c790902d90f389eafc1983a2
Roman Mashin <roman-mashin@yandex.ru>
2023-06-02 02:49:43 +0500

    update redux, storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,7 @@ module.exports = {
         es2021: true,
         jest: true,
     },
-    extends: [
-        'plugin:react/recommended',
-        'airbnb',
-        'plugin:i18next/recommended',
-    ],
+    extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended', 'plugin:storybook/recommended'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaFeatures: {
@@ -17,21 +13,14 @@ module.exports = {
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
-    plugins: [
-        'react',
-        '@typescript-eslint',
-        'i18next',
-        'react-hooks',
-        'fsd-inspector',
-        'unused-imports',
-    ],
+    plugins: ['react', '@typescript-eslint', 'i18next', 'react-hooks', 'fsd-inspector', 'unused-imports'],
     rules: {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
-        'react/jsx-filename-extension': [
-            2, { extensions: ['.js', '.jsx', '.tsx'] },
-        ],
+        'react/jsx-filename-extension': [2, {
+            extensions: ['.js', '.jsx', '.tsx'],
+        }],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
         'no-unused-vars': 'warn',
@@ -43,25 +32,14 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': [
-            'error',
-            {
-                markupOnly: true,
-                ignoreAttribute: [
-                    'data-testid',
-                    'to',
-                    'target',
-                    'direction',
-                    'align',
-                    'justify',
-                    'gap',
-                    'role',
-                    'as',
-                    'border',
-                ],
-            },
-        ],
-        'max-len': ['error', { ignoreComments: true, code: 125 }],
+        'i18next/no-literal-string': ['error', {
+            markupOnly: true,
+            ignoreAttribute: ['data-testid', 'to', 'target', 'direction', 'align', 'justify', 'gap', 'role', 'as', 'border'],
+        }],
+        'max-len': ['error', {
+            ignoreComments: true,
+            code: 125,
+        }],
         'jsx-a11y/no-static-element-interactions': 'off',
         'jsx-a11y/click-events-have-key-events': 'off',
         'react-hooks/rules-of-hooks': 'error',
@@ -69,21 +47,17 @@ module.exports = {
         'no-param-reassign': 'off',
         'no-undef': 'off',
         'react/no-array-index-key': 'off',
-        'fsd-inspector/module-relative-path': ['error', { alias: '@' }],
-        'fsd-inspector/public-api-imports': [
-            'error',
-            {
-                alias: '@',
-                testFilesPatterns: ['**/*.test.*', '**/*.stories.*', '**/StoreDecorator.tsx'],
-            },
-        ],
-        'fsd-inspector/layers-restriction': [
-            'error',
-            {
-                alias: '@',
-                ignoreImportPatterns: ['**/StoreProvider', '**/testing'],
-            },
-        ],
+        'fsd-inspector/module-relative-path': ['error', {
+            alias: '@',
+        }],
+        'fsd-inspector/public-api-imports': ['error', {
+            alias: '@',
+            testFilesPatterns: ['**/*.test.*', '**/*.stories.*', '**/StoreDecorator.tsx'],
+        }],
+        'fsd-inspector/layers-restriction': ['error', {
+            alias: '@',
+            ignoreImportPatterns: ['**/StoreProvider', '**/testing'],
+        }],
         'unused-imports/no-unused-imports': 'error',
     },
     globals: {
--- config/storybook/main.js
+++ config/storybook/main.js
@@ -1,21 +1,16 @@
 module.exports = {
-    stories: [
-        '../../src/**/*.stories.@(js|jsx|ts|tsx)',
-    ],
-    addons: [
-        '@storybook/addon-links',
-        {
-            name: '@storybook/addon-essentials',
-            options: {
-                backgrounds: false,
-            },
+    stories: ['../../src/**/*.stories.@(js|jsx|ts|tsx)'],
+    addons: ['@storybook/addon-links', {
+        name: '@storybook/addon-essentials',
+        options: {
+            backgrounds: false,
         },
-        '@storybook/addon-interactions',
-        'storybook-addon-mock/register',
-        'storybook-addon-themes',
-    ],
-    framework: '@storybook/react',
-    core: {
-        builder: '@storybook/builder-webpack5',
+    }, '@storybook/addon-interactions', 'storybook-addon-mock', 'storybook-addon-themes'],
+    framework: {
+        name: '@storybook/react-webpack5',
+        options: {},
+    },
+    features: {
+        storyStoreV7: false,
     },
 };
